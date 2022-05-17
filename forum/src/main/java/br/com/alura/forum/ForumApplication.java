0741e3f89f423ed35d6ae36f6efc71b57e73c7a3
Alisson Pazze <alisson_pazze@sicredi.com.br>
2022-05-17 20:53:28 -0300

    Add outra forma de uso de Pageable por parâmetro do método lista

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport // para funcionar atributo paginação em lista de tópicos
 public class ForumApplication {
 
 	public static void main(String[] args) {
